77036085c763a03776467c77cdf929b854034a7a
ArnonAsquira <arnonafking2010@gmail.com>
2021-11-04 11:40:20 +0200

    creating a validator for custom url ending

--- public/ELforStatisticsShett.js
+++ public/ELforStatisticsShett.js
@@ -6,7 +6,7 @@ const serverUrl = 'http://localhost:3000';
 // searrch url statistics in the server
 export async function searchUrlStatistics(e) {
     e.preventDefault();
-    const response = await axios.get(`http://localhost:3000/api/statistic/${globalVR.searchStatisticsInput.value.slice(30)}`);
+    const response = await axios.get(`${serverUrl}/api/statistic/${globalVR.searchStatisticsInput.value.slice(30)}`);
     console.log(response.data);
     const urlTimesUsed = createStatsEntries('times used:', response.data.timesUsed);
     const urlCreateDate = createStatsEntries('shortened url created at:', response.data.dateCreated.slice(0, 9));
@@ -21,6 +21,9 @@ function createStatsEntries(entry, stat) {
 // creates a url object to be sent to the server via the body's request
 function createUniqueUrl(url, ending) {
     try {
+        ending = ending.split('').filter(char =>
+            char !== " " 
+        ).join('');
         const urlObj = {url, ending};
         return urlObj;
     }catch (error) {
@@ -31,10 +34,8 @@ function createUniqueUrl(url, ending) {
 export async function sendUniqueUrlObjToServer(e) {
     e.preventDefault();
     const urlPremiumEntry = createUniqueUrl(globalVR.createUniqueUrlOldInput.value, globalVR.createUniqueUrlNewInput.value);
-    console.log(urlPremiumEntry);
     try {
         const response = await axios.post(`${serverUrl}/api/shorturl/`, urlPremiumEntry);
-        console.log(response)
         helpers.displayUrl(response.data.newUrl);
     } catch(error) {
         console.log(error);
