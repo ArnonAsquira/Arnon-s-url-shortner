d804824fc825bc43b34e018871b41dd6de255b91
ArnonAsquira <arnonafking2010@gmail.com>
2022-01-13 09:27:04 +0200

    a

--- tests/main.test.js
+++ tests/main.test.js
@@ -1,79 +1,47 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require("puppeteer");
 
-let browser;
-let page;
-let firstUrl;
-let newUrl; 
+const baseUrl = "http://localhost:3000/";
 
-async function start(urlTOenter) {
-   browser = await puppeteer.launch({headless: false, slowMo: 10});
-   page = await browser.newPage();
-   await page.goto('http://localhost:3000/');
-   await page.type('#url_input', urlTOenter);
-   await page.waitForTimeout(10);
-   await page.click('#submit-url');
-   await page.waitForTimeout(10);
-   page.on('dialog', async dialog => {
-      await dialog.dismiss();
-   });
-   newUrl = await page.evaluate(async () => {
-      try{
-         return document.querySelector('body > main > div').textContent;
-      } catch(error) {
-         return newUrl = null;
-      }
-   })
-   await browser.close()
-}
+// let browser;
+// let page;
+// let firstUrl;
+// let newUrl;
 
-let pageUrl;
-let customEnding;
-async function redirect() {
-   browser = await puppeteer.launch({headless: false, slowMo: 10});
-   page = await browser.newPage();
-   await page.goto('http://localhost:3000/');
-   await Promise.all([page.click('body > nav > ul > li:nth-child(3)'),  page.waitForNavigation()]);
-   await page.waitForTimeout(10);
-   pageUrl = await page.url();
-   await page.type('#root > div > form > input[type=text]:nth-child(2)', 'https://www.youtube.com/watch?v=Oe421EPjeBE');
-   await page.type('#root > div > form > input[type=text]:nth-child(3)', 'custom');
-   await page.click('#root > div > form > button');
-   await page.waitForTimeout(10);
-   customEnding = await page.evaluate(async () => {
-      try{
-         return document.querySelector('body > main > div.newUrl-display-div').textContent;
-      } catch(error) {
-         return newUrl = null;
-      }
-   })
-   await browser.close()
-}
+const newUrl = async (url) => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
 
+  await page.goto(baseUrl);
+  await page.type("#url_input", url);
+  await page.click("#submit-url");
+  await page.waitForTimeout(1500);
+  const newUrl = await page.evaluate(() => {
+    return document.querySelector(".newUrl-display-div").textContent;
+  });
+  browser.close();
+  return newUrl;
+};
 
-afterEach(async () => {
-   await browser.close();
- })
+const mockData = async (url, querySelector) => {
+  const browser = await puppeteer.launch({ headless: false });
+  const page = await browser.newPage();
 
+  await page.goto(url);
 
-test('entering a invalid url should resault in an alert', async () => {
-   await start('invalid url');
-   expect(newUrl).toBe(null);
+  const data = await page.evaluate((Selector) => {
+    return document.querySelector(Selector).textContent;
+  }, querySelector);
+  await browser.close();
+  return data;
+};
+
+describe("entering the google adress", () => {
+  test("should return a shortened url that leades to the goole website", async () => {
+    expect(
+      await mockData(
+        await newUrl("https://en.wikipedia.org/wiki/Java"),
+        "#firstHeading"
+      )
+    ).toBe("Java");
+  }, 8000);
 });
-
-test('entering a valid url should rsault in getting a shorten url', async() => {
-   await start('https://www.sport5.co.il/liga.aspx?FolderID=397');
-   firstUrl = newUrl;
-   expect(Boolean(newUrl)).toBe(true);
-});
-
-test('entering the same url should resault in the same shortened url', async() => {
-   await start('https://www.sport5.co.il/liga.aspx?FolderID=397');
-   expect(newUrl).toBe(firstUrl);
-})
-
-test('pressing on the nav bar links should redirect to a new page', async() => {
-    await redirect();
-    expect(pageUrl).toBe('http://localhost:3000/satistics');
-    expect(customEnding).toBe('http://localhost:3000/arniurl/custom');
-})
-
